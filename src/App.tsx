9205ef5e88f2dd196132c5812a482c4900c352bf
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-01-10 16:04:03 +0600

    useReducer() done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
+// import { useState } from "react";
 // import CounterWithClass from "./components/CounterWithClassComponent";
-import CounterWithFuncComponent from "./components/CounterWithFuncComponent";
-import UserInfoWithUseState from "./components/UserInfoWithUseState";
+// import CounterWithFuncComponent from "./components/CounterWithFuncComponent";
+// import UserInfoWithUseState from "./components/UserInfoWithUseState";
+import UserInfoWithUseReducer from "./components/UserInfoWithUseReducer";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <div className="border border-purple-500 p-10 m-10">
       {/* <h1>Count = {count}</h1> */}
       {/* <CounterWithClass /> */}
       {/* <CounterWithFuncComponent count={count} setCount={setCount} /> */}
-      <UserInfoWithUseState />
+      <UserInfoWithUseReducer />
     </div>
   );
 }
